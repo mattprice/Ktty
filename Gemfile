5bcef3a09c45fc9004206a11dcce2183c28cdace
Matthew Price <hello@mattprice.me>
2013-01-11 19:56:43 -0500

    Parse out language and code from the Gist API response.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source :rubygems
 
 gem 'sinatra', '~> 1.3.3'
+gem 'htmlentities'
 
 group :development, :test do
   gem 'sinatra-contrib'
--- ktty.rb
+++ ktty.rb
@@ -1,6 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader' unless ENV['RACK_ENV'] == 'production'
 
+require 'open-uri'
+require 'htmlentities'
+
 get '/' do
-  'Hello, world!'
+   html = ""
+
+   # https://api.github.com/gists/5ba8b745963d9f89683b
+   gist = open('http://cdn.mattprice.me/gists/5ba8b745963d9f89683b') do |data|
+      JSON.parse(data.read)
+   end
+
+   gist['files'].each { |file|
+      content  = HTMLEntities.new.encode file[1]['content']
+      language = file[1]['language']
+
+      html << "RACK_ENV: #{ENV['RACK_ENV']} <br>\n"
+      html << "Language: #{language} <br>\n"
+      html << "<pre><code class=\"language-#{language}\">#{content}</code></pre>"
+   }
+
+   html
 end
