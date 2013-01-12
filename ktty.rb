b2564cc0182a7415cd39d6e1226b17d20101c9ea
Matthew Price <hello@mattprice.me>
2013-01-12 18:33:05 -0500

    Add Prism and boilerplate templating.

--- ktty.rb
+++ ktty.rb
@@ -8,18 +8,28 @@ require 'open-uri'
 get '/' do
    html = ""
 
-   # https://api.github.com/gists/5ba8b745963d9f89683b
-   gist = open('http://cdn.mattprice.me/gists/5ba8b745963d9f89683b') do |data|
+   # Request the Gist from the GitHub API.
+   gist = open('http://cdn.mattprice.me/gists/4520261') do |data|
       JSON.parse(data.read)
    end
 
-   gist['files'].each { |file|
-      content  = HTMLEntities.new.encode file[1]['content']
-      language = file[1]['language']
+   html << "<html><head>"
+   html << "<title>#{gist['description'].strip}</title>"
+   html << "<link href='prism.css' rel='stylesheet' />"
+   html << "</head><body>\n"
 
-      html << "Language: #{language} <br>\n"
-      html << "<pre><code class=\"language-#{language}\">#{content}</code></pre>"
+   # Gists can contain multiple files so loop through each one.
+   gist['files'].each { |file|
+      file = file[1]
+
+      content  = HTMLEntities.new.encode file['content']
+      language = file['language'].downcase
+
+      html << "<pre><code class=\"language-#{language}\">#{content}</code></pre>\n"
    }
+
+   html << "<script src='prism.js'></script>"
+   html << "</body></html>"
 
    html
 end
