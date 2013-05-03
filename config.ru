5964ab1ae4fd1caeb2bcc679ab26d259dfdb4d7a
Matthew Price <hello@mattprice.me>
2013-05-02 22:39:08 -0400

    Switch from classic-style Sinatra to modular Sinatra.

--- config.ru
+++ config.ru
@@ -4,4 +4,4 @@ require 'bundler'
 Bundler.require
 
 require './ktty'
-run Sinatra::Application
+run Ktty
--- ktty.rb
+++ ktty.rb
@@ -1,94 +1,106 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/reloader' if development?
 
 require 'htmlentities'
 require 'json'
 require 'open-uri'
 
-# Some languages share the same Prism highlighting component, at least for now.
-def get_class(language)
-   class_name = {
-      'c#'          => 'clike',
-      'c++'         => 'clike',
-      'd'           => 'clike',
-      'go'          => 'clike',
-      'html'        => 'markup',
-      'json'        => 'javascript',
-      'objective-c' => 'clike',
-      'perl'        => 'clike',
-      'php'         => 'clike',
-      'rust'        => 'clike',
-      'vala'        => 'clike',
-   }
+class Ktty < Sinatra::Base
+   set :root  , File.dirname(__FILE__)
+   set :static, true
 
-   language = language.downcase
-   class_name[language] || language
-end
+   register Sinatra::Reloader
 
-# Some languages may extend a base language (e.g., JavaScript extends "clike").
-# Some languages may contain others (e.g., HTML can contain JavaScript or CSS).
-def get_dependencies(language)
-   extends = {
-      'javascript' => ['clike'],
-      'java'       => ['clike'],
-   }
+   # Some languages share the same Prism highlighting component, at least for now.
+   def get_class(language)
+      class_name = {
+         'c#'          => 'clike',
+         'c++'         => 'clike',
+         'd'           => 'clike',
+         'go'          => 'clike',
+         'html'        => 'markup',
+         'json'        => 'javascript',
+         'objective-c' => 'clike',
+         'perl'        => 'clike',
+         'php'         => 'clike',
+         'rust'        => 'clike',
+         'vala'        => 'clike',
+      }
 
-   contains = {
-      'markup' => ['css', 'javascript'],
-   }
-
-   r  = extends[language] || []
-   r += [language]
-   r += contains[language] || []
-end
-
-get '/' do
-   "Hello, world!"
-end
-
-get '/g/' do
-   redirect '/', 301
-end
-
-get '/g/:snippet' do |id|
-   url = development? ? "/static" : "http://static.ktty.co"
-   api = development? ? "http://cdn.mattprice.me/gists" : "https://api.github.com/gists"
-
-   # Request the Gist from the GitHub API.
-   # TODO: Need to handle 404 errors.
-   gist = open("#{api}/#{id}") do |data|
-      JSON.parse(data.read)
+      language = language.downcase
+      class_name[language] || language
    end
 
-   html  = "<!doctype html>"
-   html << "<html><head>"
-   html << "<title>#{gist['description'].strip}</title>"
-   html << "<link href ='#{url}/ktty.css' rel='stylesheet' />"
-   html << "<link href='http://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet' type='text/css'>"
-   html << "</head><body>\n"
+   # Some languages may extend a base language (e.g., JavaScript extends "clike").
+   # Some languages may contain others (e.g., HTML can contain JavaScript or CSS).
+   def get_dependencies(language)
+      extends = {
+         'javascript' => ['clike'],
+         'java'       => ['clike'],
+      }
 
-   # Gists can contain multiple files so loop through each one.
-   dependencies = []
-   gist['files'].each { |file|
-      file = file[1]
+      contains = {
+         'markup' => ['css', 'javascript'],
+      }
 
-      content       = HTMLEntities.new.encode file['content']
-      language      = get_class file['language']
-      dependencies += get_dependencies language
+      r  = extends[language] || []
+      r += [language]
+      r += contains[language] || []
+   end
 
-      html << "<pre data-linenums=\"1\"><code class=\"language-#{language}\">#{content}</code></pre>\n"
-   }
+   get '/' do
+      "Hello, world!"
+   end
 
-   # Include specific language files.
-   # TODO: This should probably check to see if we support the language.
-   html << "<script src='#{url}/prism.min.js'></script>"
-   dependencies.uniq.each { |language|
-      html << "<script src='#{url}/#{language}.min.js'></script>"
-   }
-   html << "<script src='#{url}/show-invisibles.min.js'></script>"
-   html << "<script src='#{url}/linenums.min.js'></script>"
+   get '/g/' do
+      redirect '/', 301
+   end
 
-   html << "</body></html>"
+   get '/g/:snippet' do |id|
+      if development?
+         url = "/static"
+         api = "http://mattprice.me/gists"
+      else
+         url = "http://static.ktty.co"
+         api = "https://api.github.com/gists"
+      end
 
-   html
+      # Request the Gist from the GitHub API.
+      # TODO: Need to handle 404 errors.
+      gist = open("#{api}/#{id}") do |data|
+         JSON.parse(data.read)
+      end
+
+      html  = "<!doctype html>"
+      html << "<html><head>"
+      html << "<title>#{gist['description'].strip}</title>"
+      html << "<link href ='#{url}/ktty.css' rel='stylesheet' />"
+      html << "<link href='http://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet' type='text/css'>"
+      html << "</head><body>\n"
+
+      # Gists can contain multiple files so loop through each one.
+      dependencies = []
+      gist['files'].each { |file|
+         file = file[1]
+
+         content       = HTMLEntities.new.encode file['content']
+         language      = get_class file['language']
+         dependencies += get_dependencies language
+
+         html << "<pre data-linenums=\"1\"><code class=\"language-#{language}\">#{content}</code></pre>\n"
+      }
+
+      # Include specific language files.
+      # TODO: This should probably check to see if we support the language.
+      html << "<script src='#{url}/prism.min.js'></script>"
+      dependencies.uniq.each { |language|
+         html << "<script src='#{url}/#{language}.min.js'></script>"
+      }
+      html << "<script src='#{url}/show-invisibles.min.js'></script>"
+      html << "<script src='#{url}/linenums.min.js'></script>"
+
+      html << "</body></html>"
+
+      html
+   end
 end
