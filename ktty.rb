fe0765508425fd70e2207a962f76426eb60d98dd
Matthew Price <hello@mattprice.me>
2013-08-01 23:26:05 -0400

    Add example syntax.

--- ktty.rb
+++ ktty.rb
@@ -21,7 +21,7 @@ class Ktty < Sinatra::Base
   end
 
   get '/' do
-    'Hello, world!'
+    'Syntax is http://ktty.co/g/gist_id. Example: <a href="http://www.ktty.co/g/5ba8b745963d9f89683b">http://www.ktty.co/g/5ba8b745963d9f89683b</a>'
   end
 
   not_found do
