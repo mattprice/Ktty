b46afc6141e82d10065cd8652e79f6e7576ceef5
Matthew Price <hello@mattprice.me>
2014-08-26 18:46:46 -0400

    Add styling for FileShuttle uploads.

--- fileshuttle.rb
+++ fileshuttle.rb
@@ -3,6 +3,12 @@ require './ktty'
 class FileShuttle < Ktty
   # Send the file directly
   get '/f/:file' do |file|
-    send_file File.join('f', file)
+    @image_path = file
+    haml :fileshuttle
+  end
+
+  # Fetch the file.
+  get '/embed/:file' do |file|
+    send_file File.join('public_html/embed', file)
   end
 end
