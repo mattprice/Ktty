7be0140a52fde4d7a677ef1f5bd2da530dee3b2a
Matthew Price <hello@mattprice.me>
2014-08-28 21:05:39 -0400

    Return 404 if a file upload does not exist.

--- app/routes/uploads.rb
+++ app/routes/uploads.rb
@@ -1,11 +1,21 @@
 module Ktty
   module Routes
     class Uploads < Base
+      def file_url(filename)
+        "/uploads/" + filename
+      end
+
+      def file_path(filename)
+        "../../../public/" + file_url(filename)
+      end
+
       get '/f/:file' do |file|
+        error 404 unless File.exist? File.expand_path(file_path(file), __FILE__)
+
         if file =~ /\.(jpe?g|png|gif)$/i
           haml :image_upload, locals: {image: file}
         else
-          redirect to("/uploads/#{file}"), 302
+          redirect to file_url(file), 302
         end
       end
     end
